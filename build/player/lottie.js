c33a0a49319a1023f4a5ef80633137be37848d13
Dolev Bitran <DolevBitran@Gmail.com>
2021-10-17 11:17:51 +0300

    suggested fix

--- build/player/lottie.js
+++ build/player/lottie.js
@@ -9039,6 +9039,7 @@ SVGBaseElement.prototype = {
   },
   createRenderableComponents: function () {
     this.maskManager = new MaskElement(this.data, this, this.globalData);
+    if (Object.keys(this.effectsManager).length === 0) return;
     this.renderableEffectsManager = new SVGEffects(this);
   },
   setMatte: function (id) {
